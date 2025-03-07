b500fa183a0faed7d5bafa9481de998b2afc98a9
Ahmad Hamza <priahmz@gmail.com>
2025-03-07 12:42:06 +0100

    Update approve-payment.js

--- api/approve-payment.js
+++ api/approve-payment.js
@@ -1,5 +1,5 @@
 export default async function handler(req, res) {
-    if (req.method !== 'POST') {
+    if (req.method !== 'GET') {
         return res.status(405).json({ success: false, error: 'Method not allowed' });
     }
 
