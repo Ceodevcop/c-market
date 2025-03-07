737bb27460cfe59c02d1f3f959a64b7be8347053
Ahmad Hamza <priahmz@gmail.com>
2025-03-07 02:21:54 +0100

    Update complete-payment.js

--- api/complete-payment.js
+++ api/complete-payment.js
@@ -28,4 +28,4 @@ export default async function handler(req, res) {
         console.error('Completion error:', error);
         res.status(500).json({ success: false, error: error.message });
     }
-          }
+}
