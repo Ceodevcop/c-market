7806069e8bf148449af568c20b25c3ef5d22df8f
Ahmad Hamza <priahmz@gmail.com>
2025-03-08 02:53:16 +0100

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -24,7 +24,7 @@ document.getElementById("payPi").addEventListener("click", async () => {
 
         // Send transaction to backend
         fetch("/api/verify_payment", {
-            method: "GET",
+            method: "POST",
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify({ txid: payment.txid })
         })
